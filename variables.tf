82bee1b7a65a9a746bf887200530268a759c7fe8
anshumanmishra3 <100398235+anshumanmishra3@users.noreply.github.com>
2023-02-28 16:11:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "layer_name" {
+  type        = string
+  description = "The name of the Lambda Layer to create"
+  default = 'layer'
+}
 
+variable "s3_bucket" {
+  type        = string
+  description = "The name of the S3 bucket where the ZIP file is stored"
+}
+
+variable "s3_key" {
+  type        = string
+  description = "The key of the ZIP file in the S3 bucket"
+}
