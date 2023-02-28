cb29cfb5a99187c30e5e11c67a9375eaf41c8165
anshumanmishra3 <100398235+anshumanmishra3@users.noreply.github.com>
2023-02-28 16:20:24 +0530

    Update output.tf

--- modules/output.tf
+++ modules/output.tf
@@ -1,3 +1,8 @@
-output "layer_arn" {
-  value = aws_lambda_layer_version.layer.arn
+# output "layer_arn" {
+#   value = aws_lambda_layer_version.layer.arn
+# }
+
+output "lambda_functions" {
+  value = { for c in var.function_configurations : c.function_name => aws_lambda_function.lambda_functions[c.function_name] }
 }
+
